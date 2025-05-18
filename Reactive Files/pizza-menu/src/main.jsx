f83c3ec93f1044eb018b69fbf78cb44dc03b76eb
Gautham <gauthamskaniyattil@gmail.com>
2025-05-18 14:46:52 +0530

    React refresh

--- Reactive Files/pizza-menu/src/main.jsx
+++ Reactive Files/pizza-menu/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+function App() {
+  return <h1>Hello World!</h1>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
