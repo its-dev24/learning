a3179948a699da40ae18167dea182c25c5521b1d
Gautham <gauthamskaniyattil@gmail.com>
2025-05-18 20:47:05 +0530

    Day 1

--- Reactive Files/pizza-menu/src/main.jsx
+++ Reactive Files/pizza-menu/src/main.jsx
@@ -1,8 +1,113 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./main.css";
+
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
 
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>Pizza Company!</h1>
+    </header>
+  );
+};
+
+const Menu = () => {
+  return (
+    <main className="menu">
+      <h2>Our Menu</h2>
+      <Pizza
+        name="Pizza Spinaci"
+        photoName="pizzas/spinaci.jpg"
+        price={12}
+        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+      />
+      <Pizza
+        name="Pizza Prosciutto"
+        ingredients="Tomato, mozarella, ham, aragula, and burrata cheese"
+        price={18}
+        photoName="pizzas/prosciutto.jpg"
+      />
+      <Pizza />
+    </main>
+  );
+};
+
+const Footer = () => {
+  const hour = new Date().getHours();
+  const isOpen = hour >= 12 && hour <= 17;
+  const footerString = isOpen ? `We are currently Open` : `Sorry we are closed`;
+  return (
+    <footer className="footer">
+      {new Date().toLocaleTimeString()} : {footerString}
+    </footer>
+  );
+};
+
+function Pizza(props) {
+  const { name, photoName, price, ingredients } = props;
+  return (
+    <div className="pizza">
+      <img src={photoName} alt={name} />
+      <div>
+        <h3>{name}</h3>
+        <p>{ingredients}</p>
+        <span>{price}</span>
+      </div>
+    </div>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
